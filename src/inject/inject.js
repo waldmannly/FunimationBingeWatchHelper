6fd01ac6288afe9478e71b337fb8b2e80c1e7e8f
unknown <waldmannevan@knights.ucf.edu>
2020-08-16 16:51:25 -0400

    updates to make youtube ads skip better (seperated from funimation functionalliy and adding more options with polling times and such. Fixed full screen- window has to be clicked in for it to work

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,34 +1,38 @@
 chrome.extension.sendMessage({}, function (response) {
 
-	var first = true; 
-
 	var readyStateCheckInterval = setInterval(function () {
-		console.log("here"); 
+		console.log("funimation binge watch helper loaded."); 
 		if (document.readyState === "complete") {
-			if (window.location.href.indexOf('youtube') > 0) { 
-				var elements = document.getElementsByClassName("ytp-ad-skip-button");
-				if (elements.length > 0) {
-					console.log("attempting to skip youtube ad")
-					console.log(elements[0].click());
-				}
-			}
+			// if (window.location.href.indexOf('youtube') > 0 && youtubeSkip === false) { 
+			// 	var elements = document.getElementsByClassName("ytp-ad-skip-button");
+			// 	if (elements.length > 0) {
+			// 		console.log("attempting to skip youtube ad")
+			// 		console.log(elements[0].click());
+			// 	}
+			// }
 			clearInterval(readyStateCheckInterval);
 			
 			var name = chrome.storage.sync.get({
-				pollingInt: 1500,
+				youtubeSkip: true, 
+				funimationSkip: true,
+				youtubePollingInt: 1500,
+				funimationPollingInt: 5000,
 				introInt: 3,
 				jumpInt: 22.5,
 				hideLogo: true,
-				hideTime: true
-				//autoFull: false
+				hideTime: true,
+				autoFull: false
 			}, function (items) {
-				pollingInt = items.pollingInt;
+				youtubeSkip = items.youtubeSkip;
+				funimationSkip = items.funimationSkip;
+				youtubePollingInt= items.youtubePollingInt;
+				funimationPollingInt= items.funimationPollingInt;
 				introInt = items.introInt;
 				jumpInt = items.jumpInt;
 				hideLogo = items.hideLogo; 
 				hideTime = items.hideTime; 
-				//autoFull = items.autoFull; 
-				if (window.location.href.indexOf('youtube') > 0 && document) {
+				autoFull = items.autoFull; 
+				if (window.location.href.indexOf('youtube') > 0 && document && youtubeSkip) {
 					var elements = document.getElementsByClassName("ytp-ad-skip-button");
 					if (elements.length>0) {
 						console.log("attempting to skip youtube ad")
@@ -36,67 +40,104 @@ chrome.extension.sendMessage({}, function (response) {
 					}
 				}
 				
-				var test = setInterval(function () {
-
-					if (document && document.getElementById("player") == null) return;
-					chrome.storage.sync.get({
-						pollingInt: 5000,
-						introInt: 3,
-						jumpInt: 22.5,
-						hideLogo: true, 
-						hideTime: true
-						//autoFull: false 
-					}, function (items) {
-							pollingInt = items.pollingInt;
-							introInt = items.introInt;
-							jumpInt = items.jumpInt;
-							hideLogo = items.hideLogo;
-							hideTime = items.hideTime;
-							//autoFull = items.autoFull; 
-					});
-
-					if (window.location.href.indexOf('youtube') > 0 && document) {
-						var elements = document.getElementsByClassName("ytp-ad-skip-button");
-						if (elements.length > 0) {
-							console.log("attempting to skip youtube ad")
-							console.log(elements[0].click());
+				if (window.location.href.indexOf('youtube') > 0){
+					var youtubePoller = setInterval(function () {
+						if (!hideTime) {
+							console.log("in utube poller"); 
 						}
-						return; 
-					}
-					curTime = document.getElementById("player").contentWindow.document.getElementById("brightcove-player_html5_api").currentTime / 60;
-					if (curTime < introInt) {
-						//if (autoFull && first) {
-						//	document.getElementById("player").contentWindow.document.getElementById("funimation-control-fullscreen").click();
-						//	first = false;
-						//}
-						maxPress = Math.ceil(introInt * 60 - curTime * 60) / 10.0;
-						console.log(maxPress)
-						for (var i = 0; i < maxPress; i++) {
-							document.getElementById("player").contentWindow.document.getElementById("funimation-control-forward").click();
-							console.log("Press");
+						if ((document && document.getElementById("player") == null )) return;
+						// chrome.storage.sync.get({
+						// 	youtubeSkip: true, 
+						// 	youtubePollingInt: 1500,
+						// 	hideTime: true
+						// }, function (items) {
+						// 	youtubeSkip = items.youtubeSkip;
+						// 	youtubePollingInt= items.youtubePollingInt;
+						// 	hideTime = items.hideTime;
+						// });
+
+						if (window.location.href.indexOf('youtube') > 0 && document && youtubeSkip) {
+							var elements = document.getElementsByClassName("ytp-ad-skip-button");
+							if (elements.length > 0) {
+								console.log("attempting to skip youtube ad")
+								console.log(elements[0].click());
+							}
+							return; 
 						}
+						if (youtubeSkip === false){
+							console.log("clear youtube poller")
+							clearInterval(youtubePoller);
+						}
+					}, youtubePollingInt);
+				}
 
-					}
+				if (window.location.href.indexOf('funimation') > 0){
+					var funimationPoller = setInterval(function () {
+						if (!hideTime) {
+							console.log("in fun poller"); 
+						}
+						if ((document && document.getElementById("player") == null )) return;
+						// chrome.storage.sync.get({
+						// 	funimationSkip: true,
+						// 	funimationPollingInt: 5000,
+						// 	introInt: 3,
+						// 	jumpInt: 22.5,
+						// 	hideLogo: true,
+						// 	hideTime: true
+						// 	//autoFull: false
+						// }, function (items) {
+						// 	funimationSkip = items.funimationSkip;
+						// 	funimationPollingInt= items.funimationPollingInt;
+						// 	introInt = items.introInt;
+						// 	jumpInt = items.jumpInt;
+						// 	hideLogo = items.hideLogo; 
+						// 	hideTime = items.hideTime; 
+						// 	//autoFull = items.autoFull; 
+						// });
+						if (funimationSkip === false){
+							console.log("clear fun poller")
+							clearInterval(funimationPoller);
+						}
+						if (funimationSkip)
+						{
+							curTime = document.getElementById("player").contentWindow.document.getElementById("brightcove-player_html5_api").currentTime / 60;
+							if (curTime < introInt) {
+								//if (autoFull && first) {
+								//	document.getElementById("player").contentWindow.document.getElementById("funimation-control-fullscreen").click();
+								//	first = false;
+								//}
+								maxPress = Math.ceil(introInt * 60 - curTime * 60) / 10.0;
+								console.log(maxPress)
+								for (var i = 0; i < maxPress; i++) {
+									document.getElementById("player").contentWindow.document.getElementById("funimation-control-forward").click();
+									console.log("Press");
+								}
 
-					//if (autoFull && first) {
-					//	document.getElementById("player").contentWindow.document.getElementById("funimation-control-fullscreen").click();
-					//	first = false;
-					//}
+							}
 
-					if (!hideTime) {
-						console.log(curTime);
-					}
+							if (autoFull && !(document.fullscreenElement) ) {
+								// document.getElementById("player").contentWindow.document.getElementById("funimation-control-fullscreen").click();
+								// first = false;
+								document.getElementById("player").webkitRequestFullscreen();
+							}
 
-					if (curTime > jumpInt) {
-						document.getElementById("player").contentWindow.document.getElementById("funimation-control-next").click();
-					}
+							
+							if (!hideTime) {
+								console.log(curTime);
+							}
 
-					if (hideLogo) {
-						document.getElementById("player").contentWindow.document.getElementById("funimation-logo").style.display = "None";
-					}
+							if (curTime > jumpInt) {
+								document.getElementById("player").contentWindow.document.getElementById("funimation-control-next").click();
+								clearInterval(funimationPoller);
+								console.log("skipped and cleared interval")
+							}
 
-
-				}, pollingInt);
+							if (hideLogo) {
+								document.getElementById("player").contentWindow.document.getElementById("funimation-logo").style.display = "None";
+							}
+						}
+					}, funimationPollingInt);
+				}
 			});
 			console.log("skipper loaded from scripts/inject.js");
 		}
--- src/options/options.js
+++ src/options/options.js
@@ -1,18 +1,24 @@
 // Saves options to chrome.storage
 function save_options() {
-    var poll = document.getElementById('pollingInt').value;
+    var youtubeSkip = document.getElementById('youtubeSkip').checked;
+    var funimationSkip = document.getElementById('funimationSkip').checked;
+    var youtubePoll = document.getElementById('youtubePollingInt').value;
+    var funimationPoll = document.getElementById('funimationPollingInt').value;
     var intro = document.getElementById('introInt').value;
     var jump = document.getElementById('jumpInt').value;
     var hide = document.getElementById('hideLogo').checked;
     var time = document.getElementById('hideTime').checked;
-    //var full = false; //document.getElementById('autoFull').checked;
+    var full = document.getElementById('autoFull').checked;
     chrome.storage.sync.set({
-        pollingInt: poll,
+        youtubeSkip: youtubeSkip,
+        funimationSkip: funimationSkip,
+        youtubePollingInt: youtubePoll,
+        funimationPollingInt: funimationPoll,
         introInt: intro,
         jumpInt: jump, 
         hideLogo: hide,
-        hideTime: time
-        //autoFull: full
+        hideTime: time,
+        autoFull: full
     }, function () {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
@@ -28,19 +34,25 @@ function save_options() {
 function restore_options() {
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
-        pollingInt: 5000,
+        youtubeSkip: true, 
+        funimationSkip: true,
+        youtubePollingInt: 1500,
+        funimationPollingInt: 5000,
         introInt: 3,
         jumpInt: 22.5, 
         hideLogo: true, 
-        hideTime: true
-        //autoFull: false
+        hideTime: true,
+        autoFull: false
     }, function (items) {
-            document.getElementById('pollingInt').value = items.pollingInt;
+            document.getElementById('youtubeSkip').checked = items.youtubeSkip;
+            document.getElementById('funimationSkip').checked = items.funimationSkip;
+            document.getElementById('youtubePollingInt').value = items.youtubePollingInt;
+            document.getElementById('funimationPollingInt').value = items.funimationPollingInt;
             document.getElementById('introInt').value = items.introInt;
             document.getElementById('jumpInt').value = items.jumpInt;
             document.getElementById('hideLogo').checked = items.hideLogo;
             document.getElementById('hideTime').checked = items.hideTime;
-            //document.getElementById('autoFull').checked = items.autoFull;
+            document.getElementById('autoFull').checked = items.autoFull;
     });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
